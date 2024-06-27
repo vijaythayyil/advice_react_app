3f031ce3dc629a2a1c1977aeb9eb325d3d07daea
Vijay Thayyil <68293066+vijaythayyil@users.noreply.github.com>
2024-06-26 20:12:35 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+class App extends React.Component {
+  state = { advice: "" };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+        this.setState({ advice: advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { advice } = this.state;
+    return (
+      <div>
+        <h1>{advice}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
